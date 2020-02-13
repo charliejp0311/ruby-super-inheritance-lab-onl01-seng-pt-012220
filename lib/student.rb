3736f151a1aae4e87091f54c1750363ac128eab6
Charles Pelton <charliejp0311@gmail.com>
2020-02-13 14:06:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 require "pry"
 class Student
   def hello
-    "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff.\n"
   end
 
   def raise_hand
-    "Pick me!\n"
+    puts "Pick me!\n"
   end
 
 end
